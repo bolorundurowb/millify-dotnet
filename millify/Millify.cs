07754e1ecb09c515712cc775a0b8ef302da23384
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2024-06-13 09:59:57 +0100

    ft: add in millify documentation

--- millify/Millify.cs
+++ millify/Millify.cs
@@ -1,16 +1,37 @@
 ﻿namespace MillifyDotnet;
 
+/// <summary>
+/// Provides methods to shorten large numbers into a more readable format using SI units.
+/// </summary>
 public static class Millify
 {
+    /// <summary>
+    /// Shortens a long integer into a more readable format using SI units.
+    /// </summary>
+    /// <param name="number">The number to be shortened.</param>
+    /// <param name="options">The options to configure the shortening process. If null, default options will be used.</param>
+    /// <returns>A string representing the shortened number with the appropriate SI unit.</returns>
     public static string Shorten(long number, MillifyOptions? options = null) =>
         Shorten(Convert.ToDecimal(number), options);
 
+    /// <summary>
+    /// Shortens a double into a more readable format using SI units.
+    /// </summary>
+    /// <param name="number">The number to be shortened.</param>
+    /// <param name="options">The options to configure the shortening process. If null, default options will be used.</param>
+    /// <returns>A string representing the shortened number with the appropriate SI unit.</returns>
     public static string Shorten(double number, MillifyOptions? options = null) =>
         Shorten(Convert.ToDecimal(number), options);
 
+    /// <summary>
+    /// Shortens a decimal number into a more readable format using SI units.
+    /// </summary>
+    /// <param name="number">The number to be shortened.</param>
+    /// <param name="options">The options to configure the shortening process. If null, default options will be used.</param>
+    /// <returns>A string representing the shortened number with the appropriate SI unit.</returns>
     public static string Shorten(decimal number, MillifyOptions? options = null)
     {
-        options ??= new();
+        options ??= new MillifyOptions();
         var isNegative = number < 0;
 
         var absoluteValue = Math.Abs(number);
